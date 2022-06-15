86a9641a2a5571f682cd254d441827cf0995d25b
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-06-14 21:29:54 -0600

    IndexedDB completed

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -3,6 +3,7 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
 
 import { useQuery } from "@apollo/client";
+import { idbPromise } from "../../utils/helpers";
 
 import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
@@ -21,8 +22,22 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise("products", "get").then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
